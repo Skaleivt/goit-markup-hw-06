8b025236efe7b9ce279a544a032a190693b9f92e
Viktor Skalei <skaleiviktor@gmail.com>
2024-11-10 20:19:07 +0200

    hw06 03

--- js/modal.js
+++
@@ -1,6 +0,0 @@
-(() => {
-    const refs = {
-        openModalBtn: document.querySelector( selectors: '[data-menu-open]'),
-        closeModalBtn: document.querySelector( selectors: '[data-menu-close]' modal )
-    }
-})
